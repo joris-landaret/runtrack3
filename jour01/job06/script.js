65749e9174521b8a31efa490599fcce4117b753a
joris-landaret <joris.landaret@laplateforme.io>
2023-01-16 14:12:31 +0100

    job06 : ajouts fichiers index.php et script.js

---
+++ jour01/job06/script.js
@@ -0,0 +1,12 @@
+function afficherjourssemaines(){
+    
+    const jourssemaines = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]
+
+    let text = "";
+    for(let i in jourssemaines){
+        console.log(jourssemaines[i]);;
+    }
+
+}
+
+afficherjourssemaines();

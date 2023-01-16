0b75ddb5bbb2113eaaef0bced5005d3c14c7e62b
joris-landaret <joris.landaret@laplateforme.io>
2023-01-16 14:25:19 +0100

    job06 fini : ajout de la fonction fizzbuzz

--- jour01/job06/script.js
+++ jour01/job06/script.js
@@ -1,12 +1,26 @@
-function afficherjourssemaines(){
-    
-    const jourssemaines = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"]
+function fizzbuzz(){
 
-    let text = "";
-    for(let i in jourssemaines){
-        console.log(jourssemaines[i]);;
+    let x = 0;
+
+    while ( x <= 151) {
+        
+        if ( x % 3 == 0 && x % 5 == 0 ){
+            console.log("“FizzBuzz”");
+            }
+
+        else if ( x % 3 == 0 ){
+            console.log("“Fizz”");
+        }
+
+        else if ( x % 5 == 0 ){
+            console.log("“Buzz”");
+        }
+
+        else{
+            console.log(x);
+        }
+
+        x++;
     }
-
 }
-
-afficherjourssemaines();
+fizzbuzz();

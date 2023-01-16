5be82a8569901a2c964a504147b34b06dbde7ab0
joris-landaret <joris.landaret@laplateforme.io>
2023-01-16 12:14:00 +0100

    job04 fini : ajout de la fonction bisextile.

--- jour01/job04/script.js
+++ jour01/job04/script.js
@@ -1,2 +1,11 @@
-//alert("Hello Javascript!");
-console.log("Hello Javascript!")
+function bisextile(année){
+    if(année % 4 == 0 && année % 100 != 0 || année % 400 == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(bisextile(2028));
+console.log(bisextile(2400));
+console.log(bisextile(2300));

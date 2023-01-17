9bcd3434215799c043c2ae09fdfa02e938c38adc
joris-landaret <joris.landaret@laplateforme.io>
2023-01-17 11:21:54 +0100

    j2 job02 fini : création de la fonction citation

--- jour02/job01/script.js
+++ jour02/job01/script.js
@@ -1,3 +1,17 @@
-function citation(){
+document.addEventListener("DOMContentLoaded", function() {   
+    
+    // fonction qui récupère le texte de l'article et l'affiche dans la console grâce au button
+    function citation(){
+        
+        // cible l'article et le button
+        const button = document.querySelector("#button")
+        const article = document.querySelector("#citation").innerHTML
+    
+        console.log(article);
+    }
+    
+    button.addEventListener('click', citation, {
+        
+    });
 
-}
+});

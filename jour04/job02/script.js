c20e2c58c7ac88792f1bc512eb41208265c89119
joris-landaret <joris.landaret@laplateforme.io>
2023-01-19 16:11:36 +0100

    j4 job02 script.js : création fonction jsonValueKey

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,28 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {   
     
-    // cible le bouton
-    const button = document.querySelector("#button")
-    const texte = document.createElement('p');
-    document.body.append(texte)
-    let select_p = document.querySelector("p");
-    
-    //
-    function expression(){
 
-        //console.log('heu');
-        fetch('expression.txt')
-            .then( (response) => {
-                return response.text();
-                console.log(response.text());
-            })
-            .then((ok) => {
-                console.log(select_p);
-                select_p.innerHTML = ok;
-                console.log(ok);
-            })
+    //
+    function jsonValueKey(){
+
+ 
     };
     
-    button.addEventListener('click', expression, {
+    button.addEventListener('click', jsonValueKey, {
         
     });
 

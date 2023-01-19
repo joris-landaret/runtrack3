4ef2c109005c47417915187c7d6f79384e1f6180
joris-landaret <joris.landaret@laplateforme.io>
2023-01-19 11:51:38 +0100

    j3 job01 finis

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -6,17 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Document </title>
+    <script src="../../jquery-3.6.3.js"></script>
     <script src="script.js"></script>
 </head>
 
 <body>
     <main>
 
-        <button id="button">Afficher ou cacher</button>
+        <button id="button2">Afficher</button>
+        <button id="button1">Cacher</button>
 
-        <div id="div">
-
-        </div>
+        <p id="p1">Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,
+            ensuite on prie.</p>
 
     </main>
 </body>
--- jour03/job01/script.js
+++ jour03/job01/script.js
@@ -1,34 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {   
-    
-    let button = document.querySelector("#button");
-    let div = document.querySelector("#div");
-
-       
-
-    // fonction qui récupère le texte de l'article et l'affiche dans la console grâce au button
-    function showhide(){
-        
-        let article = document.createElement("article");
-        const text = "L'important n'est pas la chute, mais l'atterrissage."
-        article.textContent = text;
-        let select_article = document.querySelector("article");
-        //div.appendChild(article);
-        console.log(select_article);
-
-        if(select_article == null){
-           
-            div.appendChild(article);
-        }else {
-            console.log('toto');
-            div.removeChild(div.children[0]);
-            
-        }
-        
-        showhide.onclick;
-    }
-    
-    button.addEventListener('click', showhide, {
-        
+$(document).ready(function(){     
+    //Dès qu'on clique sur #b1, on applique hide() au titre
+    $("#button1").click(function(){
+        $("p").hide();
     });
-
+    
+    //Dès qu'on clique sur #b1, on applique show() au titre
+    $("#button2").click(function(){
+        $("p").show();
+    });
 });

c601643a2d23739f47fc80b1781d3d3bf2535cc2
joris-landaret <joris.landaret@laplateforme.io>
2023-01-19 15:49:25 +0100

    j4 job01 finis : ajout fonction affiche texte

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -6,15 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Document </title>
-    <script src="../../jquery-3.6.3.js"></script>
     <script src="script.js"></script>
 </head>
 
 <body>
     <main>
-
         <button id="button">Afficher</button>
-
+        <!-- <p id="p"></p> -->
     </main>
 </body>
 
--- jour04/job01/script.js
+++ jour04/job01/script.js
@@ -1,11 +1,29 @@
-$(document).ready(function(){     
-    //Dès qu'on clique sur #b1, on applique hide() au titre
-    $("#button1").click(function(){
-        $("p").hide();
-    });
+document.addEventListener("DOMContentLoaded", function() {   
     
-    //Dès qu'on clique sur #b1, on applique show() au titre
-    $("#button2").click(function(){
-        $("p").show();
+    // cible le bouton
+    const button = document.querySelector("#button")
+    const texte = document.createElement('p');
+    document.body.append(texte)
+    let select_p = document.querySelector("p");
+    
+    //
+    function expression(){
+
+        //console.log('heu');
+        fetch('expression.txt')
+            .then( (response) => {
+                return response.text();
+                console.log(response.text());
+            })
+            .then((ok) => {
+                console.log(select_p);
+                select_p.innerHTML = ok;
+                console.log(ok);
+            })
+    };
+    
+    button.addEventListener('click', expression, {
+        
     });
+
 });

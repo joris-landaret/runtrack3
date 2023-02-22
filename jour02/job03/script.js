2f619072cf7b040aec37a1883c18adf9da6b38b5
joris-landaret <joris.landaret@laplateforme.io>
2023-02-22 15:15:07 +0100

    jour02, job03 : selectionne bouton + p, evénement buton + créé function addone

--- jour02/job03/script.js
+++ jour02/job03/script.js
@@ -1,33 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {   
     
     let button = document.querySelector("#button");
-    let div = document.querySelector("#div");
+    let p = document.querySelector("#compteur");
 
        
-
-    // fonction qui récupère le texte de l'article et l'affiche dans la console grâce au button
-    function showhide(){
+    function addone(){
         
-        let article = document.createElement("article");
-        const text = "L'important n'est pas la chute, mais l'atterrissage."
-        article.textContent = text;
-        let select_article = document.querySelector("article");
-        //div.appendChild(article);
-        console.log(select_article);
-
-        if(select_article == null){
-           
-            div.appendChild(article);
-        }else {
-            console.log('toto');
-            div.removeChild(div.children[0]);
-            
-        }
-        
-        showhide.onclick;
     }
     
-    button.addEventListener('click', showhide, {
+    button.addEventListener('click', addone, {
         
     });
 

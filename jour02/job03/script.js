daa13a79eef0b9fd842391ab02031b9fa3ea01a8
joris-landaret <joris.landaret@laplateforme.io>
2023-02-22 15:20:10 +0100

    jour02, job03 : ajout variable i + incrémentation i dans p

--- jour02/job03/script.js
+++ jour02/job03/script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     
     let button = document.querySelector("#button");
     let p = document.querySelector("#compteur");
-
+    let i = 0;
        
     function addone(){
-        
+        p.innerHTML = i++;
     }
     
     button.addEventListener('click', addone, {

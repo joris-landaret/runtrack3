d04594ec04cdaf2abd4de3e4e4329b25243c664f
joris-landaret <joris.landaret@laplateforme.io>
2023-02-23 09:36:02 +0100

    jour04, job03 : ajout des types dans le select

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -19,8 +19,27 @@
         <label for="">nom</label>
         <input type="text">
 
-        <label for="">Type</label>
-        <select name="type" id="fg">gf</select>
+        <select name="type" id="type">
+            <option value="Type">Type</option>
+            <option value="water">Water</option>
+            <option value="Fire">Fire</option>
+            <option value="Grass">Grass</option>
+            <option value="Poison">Poison</option>
+            <option value="Flying">Flying</option>
+            <option value="Bug">Bug</option>
+            <option value="Normal">Normal</option>
+            <option value="Electric">Electric</option>
+            <option value="Ground">Ground</option>
+            <option value="Fairy">Fairy</option>
+            <option value="Fighting">Fighting</option>
+            <option value="Psychic">Psychic</option>
+            <option value="Rock">Rock</option>
+            <option value="Steel">Steel</option>
+            <option value="Ghost">Ghost</option>
+            <option value="Ice">Ice</option>
+            <option value="Dragon">Dragon</option>
+
+        </select>
 
         <button>filtrer</button>
 

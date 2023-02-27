cf5b17462ab123313cff0f9d42dd53e60fcd4d35
joris-landaret <joris.landaret@laplateforme.io>
2023-02-27 11:48:40 +0100

    petite modif

--- jour04/job03/script.js
+++ jour04/job03/script.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    let button = document.querySelector('#button');
 
+    function filtrer() {
+        
+        fetch('pokemon.json')
+        .then((response) => {
+            return response.json();
+            //console.log(response.json());
+        })
+        .then((ok) => {
+            console.log(ok);
+        })
+        .catch(error => console.log("Erreur : " + error));
+        
+    }
+
+    button.addEventListener('click', filtrer);
 
 });
